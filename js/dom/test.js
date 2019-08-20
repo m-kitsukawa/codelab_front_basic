f6177aacbc2b6e8e27a1c2e0c722ff3798dc9438
[m.kitsukawa] <[maniyachi1011@gmail.com]>
2019-08-20 10:00:52 +0900

    ファイルの更新_不明点あり

---
+++ js/dom/test.js
@@ -0,0 +1,9 @@
+// 問題1：要素の取得
+// id属性txt1の要素を取得し、変数txt1に代入してください。
+// 代入後、変数txt1をコンソールに表示してださい。
+
+var aaa = document.getElementById('txt1');
+console.log(aaa);
+
+var aaa = 'txt1';
+console.log(document.getElementById(aaa));
