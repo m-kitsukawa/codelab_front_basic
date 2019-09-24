c0df5b1efda381c1e4a986e373b093e0b9d7ae0b
[m.kitsukawa] <[maniyachi1011@gmail.com]>
2019-09-24 21:10:09 +0900

    ファイルの更新_不明点あり

---
+++ js/form_validate/common.js
@@ -0,0 +1,26 @@
+function formCheck(){
+  var flag = 0;
+
+  // 入力必須項目が入力されているかチェック
+  var name = document.getElementsByName("name");
+  if (name.value == "") {
+    var errname = document.getElementsByName("err-name");
+    errname.innerHTML = '名前が未入力です。';
+    e.preventDefault();
+    flag = 1;
+     }
+  else{
+      }
+    }
+  var submit = document.getElementsById("submit");
+  submit.addEventListener('submit', function(e){
+  if(flag){ // 入力必須項目に未入力があった場合
+      window.alert( '必須項目は全て入力して下さい。' ); // 一旦アラートを表示
+      return false; // 送信中止
+  }else{ // 入力必須項目が全て入力済みだった場合
+      return true; // 送信実行
+
+  }
+}, false);
+
+
