4e0a6c391522a883d1352e5e996b9b3ab4723dee
[m.kitsukawa] <[maniyachi1011@gmail.com]>
2020-02-01 11:20:28 +0900

    add_jquery/accordion

--- jquery/accordion/common.js
+++ jquery/accordion/common.js
@@ -1,6 +1,8 @@
 $(function(){
-  $('#accordion dt').on('click',function(){
-    // $(this).toggleClass('open');
+  $('#accordion dt').click(function(){
+    $("#accordion").children("dt")[0].click();
+    // $('#accordion dt').on('click',function(){
+      // $(this).toggleClass('open');
     $(this).next('#accordion dd').slideToggle();
     $('#accordion dt').not($(this)).next('#accordion dd').slideUp();
   });
