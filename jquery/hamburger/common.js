141f0bf1f46b1365cb126170aacd5d6aada24e43
[m.kitsukawa] <[maniyachi1011@gmail.com]>
2020-02-01 13:33:14 +0900

    fixed_jquery/hamburger

--- jquery/hamburger/common.js
+++ jquery/hamburger/common.js
@@ -1,13 +1,19 @@
-// $(".fa fa-bars").on("click", function() {
-//   if ($('body').hasClass("active")) {
-//     $('body').removeClass("active");
-//     $(".fa fa-times").slideUp();
-//   } else {
-//     $('body').addClass("active");
-//     $(".fa fa-times").slideDown();
-//   }
-// });
-
 $(".fa fa-bars").on("click", function() {
-alert("~~");
+  if ($('body').hasClass("active")) {
+    $('body').removeClass("active");
+    $(".fa fa-times").slideUp();
+  } else {
+    $('body').addClass("active");
+    $(".fa fa-times").slideDown();
+  }
 });
+// console.log('common js');
+// console.log($('.fa'));
+// $('.fa').on("click", function() {
+//   // debugger;
+//   //clickしたとき
+//   console.log('clik');
+//   //bodyタグにあくてぃぶくらすをつける
+//   $('body').addClass('active');
+//   console.log('active add');
+// });
