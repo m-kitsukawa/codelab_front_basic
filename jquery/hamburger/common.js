87e4c24c7c672af42daaba739b95594947fafb30
[m.kitsukawa] <[maniyachi1011@gmail.com]>
2020-02-01 11:51:42 +0900

    add_jquery

--- jquery/hamburger/common.js
+++ jquery/hamburger/common.js
@@ -1,3 +1,13 @@
-$('fa fa-bars').on("click", function(){
-  $('body').addClass("active");
+// $(".fa fa-bars").on("click", function() {
+//   if ($('body').hasClass("active")) {
+//     $('body').removeClass("active");
+//     $(".fa fa-times").slideUp();
+//   } else {
+//     $('body').addClass("active");
+//     $(".fa fa-times").slideDown();
+//   }
+// });
+
+$(".fa fa-bars").on("click", function() {
+alert("~~");
 });
