d9460f7b7eea8d005a12ddefa1e376a17efd1e75
[m.kitsukawa] <[maniyachi1011@gmail.com]>
2020-02-02 22:33:16 +0900

    fixed_jquery/slick

--- jquery/slick/common.js
+++ jquery/slick/common.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     // ここにオプションを追記する
      centerPadding: '30%',
      focusOnSelect: true,
-     speed: 1000
+     speed: 1000,
+    //  waitForAnimate: false,
+     autoplay: true,
+     autoplaySpeed: 5000,
+     dots: true
    });
 });
