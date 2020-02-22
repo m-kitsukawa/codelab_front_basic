34ed9e9944376383bff0bb1d25b1d30dd5655536
[m.kitsukawa] <[maniyachi1011@gmail.com]>
2020-02-22 11:37:11 +0900

    add_html/level5

--- html/level5/commmon.js
+++ html/level5/commmon.js
@@ -1,5 +1,7 @@
-$('.slider').slick({
-  autoplay:true,
-  autoplaySpeed:5000,
-  dots:true,
+$(document).ready(function() {
+  $(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true
+  });
 });
